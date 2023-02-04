a30f706de5b4bfad4d06de7324459fbfc82c201e
still <still@uab.edu>
2023-02-04 11:43:40 -0600

    update main.js
    add local storage to watchlist and delete icons, still in progress

--- frontend/static/main.js
+++ frontend/static/main.js
@@ -1,9 +1,52 @@
+// initially load the watchlist from local storage and add the stock symbols to the watchlistcontent element
+const watchlistcontent = document.getElementById("watchlist");
+let watchlist = JSON.parse(localStorage.getItem("watchlist"));
+if (watchlist !== null) {
+    for (let i = 0; i < watchlist.length; i++) {
+        watchlistcontent.innerHTML += `<li>${watchlist[i]}<i id=${watchlist[i]} class="fas fa-trash pl-3"></i></li>`;
+    }
+}
+
 // add an event listener on the submit button that reads the value submitted in the inputText Field
 const submitButton = document.getElementById("submit");
 submitButton.addEventListener("click", function(e) {
     e.preventDefault();
     const stockSymbol = document.getElementById("inputText").value;
     stockChecker(stockSymbol);
+});
+
+// add event listener to the watchlist button that adds the stock symbol to the watchlist
+const watchlistButton = document.getElementById("addtowatchlist");
+watchlistButton.addEventListener("click", function(e) {
+    console.log('watchlist button');
+    let stockSymbol = document.getElementById("inputText").value.toUpperCase();
+    // get the watchlist from local storage
+    let watchlist = JSON.parse(localStorage.getItem("watchlist"));
+    // if the watchlist is empty, create a new array and add the stock symbol to it
+    if (watchlist === null) {
+        watchlist = [];
+        watchlist.push(stockSymbol);
+        localStorage.setItem("watchlist", JSON.stringify(watchlist));
+    } else {
+        // if the watchlist is not empty, check if the stock symbol is already in the watchlist
+        if (watchlist.indexOf(stockSymbol) === -1) {
+            // if the stock symbol is not in the watchlist, add it
+            watchlist.push(stockSymbol);
+            localStorage.setItem("watchlist", JSON.stringify(watchlist));
+        } else {
+            // if the stock symbol is already in the watchlist, do nothing
+            console.log('Stock symbol already in watchlist');
+        }
+    }
+    console.log(watchlist);
+    const watchlistcontent = document.getElementById("watchlist");
+    // get the watchlist from local storage and pull out the separate stock symbols
+    // loop through the stock symbols and add them to the watchlistcontent element
+    watchlist = JSON.parse(localStorage.getItem("watchlist"));
+    watchlistcontent.innerHTML = '';
+    for (let i = 0; i < watchlist.length; i++) {
+        watchlistcontent.innerHTML += `<li>${watchlist[i]}<i id=${watchlist[i]} class="fas fa-trash pl-3"></i></li>`;
+    }
 });
 
 async function stockChecker(stockSymbol) {
@@ -69,3 +112,31 @@ async function getSMA(stockSymbol) {
         console.error(error);
     }
 }
+
+// add event listeners to all the trash icons
+// if the trash icon is clicked, remove the stock symbol from the watchlist
+const trashIcons = document.querySelectorAll(".fa-trash");
+trashIcons.forEach(function(trashIcon) {
+    trashIcon.addEventListener("click", function(e) {
+        let stockSymbol = e.target.id;
+        // get the watchlist from local storage
+        let watchlist = JSON.parse(localStorage.getItem("watchlist"));
+        // if the watchlist is not empty, check if the stock symbol is already in the watchlist
+        if (watchlist.indexOf(stockSymbol) !== -1) {
+            // if the stock symbol is in the watchlist, remove it
+            watchlist.splice(watchlist.indexOf(stockSymbol), 1);
+            localStorage.setItem("watchlist", JSON.stringify(watchlist));
+        } else {
+            // if the stock symbol is not in the watchlist, do nothing
+            console.log('Stock symbol not in watchlist');
+        }
+        const watchlistcontent = document.getElementById("watchlist");
+        // get the watchlist from local storage and pull out the separate stock symbols
+        // loop through the stock symbols and add them to the watchlistcontent element
+        watchlist = JSON.parse(localStorage.getItem("watchlist"));
+        watchlistcontent.innerHTML = '';
+        for (let i = 0; i < watchlist.length; i++) {
+            watchlistcontent.innerHTML += `<li>${watchlist[i]}<i id=${watchlist[i]} class="fas fa-trash pl-3"></i></li>`;
+        }
+    });
+});
